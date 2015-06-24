bad01977f7bfa3ea0238442e2ad36cf3379f45e4
Lakshmipriya Sivakumar <lpriya.sivakumar@gmail.com>
2015-06-24 16:42:59 -0400

    SOurce from June 24/15

---
+++ src/FutureValueApplication.java
@@ -0,0 +1,55 @@
+import java.text.NumberFormat;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class FutureValueApplication {
+
+	public static void main(String[] args) {
+		//Print header
+		System.out.println("Welcome to the Future Value Calculator\n");
+		//create an instance of Scanner
+		Scanner sc = new Scanner(System.in);
+		//Set choice value to "y" so the while loop runs at least once
+		String choice = "y";
+		while(choice.equalsIgnoreCase("y")){
+			//declare and initialize variables for monthly investment and rate of interest
+			double monthlyInvestment = 0.0;
+			double interestRate = 0.0;
+			int years = 0;
+			//Accept user input inside a try catch block
+			try{
+				System.out.print("Enter a monthly investment: ");
+				monthlyInvestment = sc.nextDouble();
+				System.out.print("Enter yearly Interest Rate: ");
+				interestRate = sc.nextDouble();
+				System.out.print("Enter number of years: ");
+				years = sc.nextInt();
+			}catch(InputMismatchException e){
+				sc.next();
+				System.out.println("Error! Invalid number. Try Again\n");
+				continue;
+			}
+			//calculate and store monthly interest rate from annual interest rate
+			//calculate number of months from years given
+			//call static method "calculateFutureValue" and pass in amount, rate and months
+			double monthlyInterestRate = interestRate/12/100;
+			int months = years*12;
+			double futureValue  = calculateFutureValue(monthlyInvestment,monthlyInterestRate,months);
+			//use NumberFormat class to format output
+			NumberFormat currency = NumberFormat.getCurrencyInstance();
+			System.out.println("Future Value: "+currency.format(futureValue)+"\n");
+			//Prompt if user wants to continue
+			System.out.print("Continue? (y/n): ");
+			choice = sc.next();
+			System.out.println();
+		}
+	}
+
+	private static double calculateFutureValue(double monthlyInvestment,
+			double monthlyInterestRate, int months) {
+		double futureValue = 0;
+		for(int i = 1; i <=months; i++){
+			futureValue
+		}
+	}
+
+}
--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -3,47 +3,52 @@ import java.util.Scanner;
 public class GuessingGame {
 
 	public static void main(String[] args) {
-		//Create an instance of Scanner
+		// Create an instance of Scanner
 		Scanner sc = new Scanner(System.in);
-		String choice = "y";//hardcoded choice as "y" so WHile loop runs atleast once
+		String choice = "y";// hardcoded choice as "y" so While loop runs at
+							// least once
 		int randomInt, guess, count;
-		
-		while(choice.equalsIgnoreCase("y")){
-			//Pick a random number
-			randomInt =(int) ((Math.random() * 100) + 1);
-			//System.out.println(randomInt);
-			//Print header and prompt
+
+		while (choice.equalsIgnoreCase("y")) {
+			// Pick a random number
+			randomInt = (int) ((Math.random() * 100) + 1);
+			// System.out.println(randomInt);
+			// Print header and prompt
 			System.out.println("Welcome to the Guessing Number Game");
 			System.out.println("++++++++++++++++++++++++++++++++++++");
 			System.out.println("I'm thinking of a number between 1 and 100");
-			//Make sure user enters an integer between 1 and 100
+			// Make sure user enters an integer between 1 and 100
 			guess = Validator.getIntInRange(sc, "Enter a guess: ", 1, 100);
-			//set count variable to 1
+			// set count variable to 1
 			count = 1;
-			//Call the compare method to compare the guess and random number if guess is wrong
-			while(guess != randomInt){
-				count ++;
+			// Call the compare method to compare the guess and random number if
+			// guess is wrong
+			while (guess != randomInt) {
+				count++;
 				compare(guess, randomInt);
-				guess = Validator.getIntInRange(sc, "Guess Again: ", 1, 100);				
+				guess = Validator.getIntInRange(sc, "Guess Again: ", 1, 100);
 			}
-			//Print the success message and number of tries it took
-			//Ask if the user wants to play again
-			System.out.printf("Great you guessed right. It took you %d tries.",count);
-			choice = Validator.getChoice(sc,"Do you want to play again? y/n: ");
+			// Print the success message and number of tries it took
+			// Ask if the user wants to play again
+			System.out.printf("Great you guessed right. It took you %d tries.",
+					count);
+			choice = Validator
+					.getChoice(sc, "Do you want to play again? y/n: ");
 		}
 		sc.close();
 		System.out.println("Thank you for playing :)");
 	}
-	//Helper method compares the guess to the number
-	//Prints specific message based on the difference between the two numbers
-	public static void compare(int guess, int number){
-		if(guess <= number-10){
+
+	// Helper method compares the guess to the number
+	// Prints specific message based on the difference between the two numbers
+	public static void compare(int guess, int number) {
+		if (guess <= number - 10) {
 			System.out.println("Way too low");
-		}else if(guess >= number + 10){
+		} else if (guess >= number + 10) {
 			System.out.println("Way too high");
-		}else if((guess < number + 10) && (guess > number)){
+		} else if ((guess < number + 10) && (guess > number)) {
 			System.out.println("High but getting closer");
-		}else if((guess > number - 10) && (guess < number)){
+		} else if ((guess > number - 10) && (guess < number)) {
 			System.out.println("Low but getting closer");
 		}
 	}
